a446a03f2e3b23d98014c3f58dd1fb632637c3b6
LastnAccount <llorenz.10022002@gmail.com>
2023-09-24 15:23:06 +0800

    word-frequency-counter

--- Practice Set 1.php
+++ Practice Set 1.php
@@ -1,28 +1,39 @@
 <?php
-// Poorly organized and hard-to-read code
-// Calculate the total price of items in a shopping cart
-$items = [
-['name' => 'Widget A', 'price' => 10],
-['name' => 'Widget B', 'price' => 15],
-['name' => 'Widget C', 'price' => 20],
-];
-$total = 0;
-foreach ($items as $item) {
-$total += $item['price'];
+function calculateTotalPrice($items)
+{
+    $total = 0;
+    foreach ($items as $item) {
+        $total += $item['price'];
+    }
+    return $total;
 }
+$items = [
+    ['name' => 'Widget A', 'price' => 10],
+    ['name' => 'Widget B', 'price' => 15],
+    ['name' => 'Widget C', 'price' => 20],
+];
+
+$total = calculateTotalPrice($items);
 echo "Total price: $" . $total;
-// Perform a series of string manipulations
+function modifyString($string)
+{
+    $string = str_replace(' ', '', $string);
+    $string = strtolower($string);
+
+    return $string;
+}
 $string = "This is a poorly written program with little
 structure and readability.";
-// Remove spaces and convert to lowercase
-$string = str_replace(' ', '', $string);
-$string = strtolower($string);
-// Display the modified string
-echo "\nModified string: " . $string;
-// Check if a number is even or odd
-$number = 42;
-if ($number % 2 == 0) {
-echo "\nThe number " . $number . " is even.";
-} else {
-echo "\nThe number " . $number . " is odd.";
+$modifiedString = modifyString($string);
+echo "\nModified string: " . $modifiedString;
+function checkEvenOrOdd($number)
+{
+    if ($number % 2 == 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
 }
+$number = 51;
+$numberType = checkEvenOrOdd($number);
+echo "\nThe number " . $number . " is " . $numberType . ".";
